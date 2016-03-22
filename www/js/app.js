a5ef1c01ba31d877f7b1a4f502f2064baf567d60
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2016-03-22 13:39:16 +0200

    Giriş Koduna göre JSON Veri Çekme İşlemleri

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,40 @@
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngResource'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
+
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
   });
 })
 
+.factory("Survey", function($http) {
+    var surveyUrl = "http://localhost:3000/surveyMobile?passcod=";
+
+    return {
+      getData: function(code, success) {
+          $http({
+                url: surveyUrl + code,
+                method: "GET"
+            }).success(function(data) {
+                    success(data);
+              });
+        }
+    }
+          
+
+})
+
 .config(function($stateProvider, $urlRouterProvider) {
+
+
   $stateProvider
 
     .state('app', {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,25 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope) {
 
-
 })
 
-.controller('MainCtrl', function($scope) {
 
+.controller('MainCtrl', function($scope, Survey) {
+
+	$scope.$root.pass_code = "";
+
+	$scope.getSurvey = function(){
+
+
+		var data = Survey.getData($scope.$root.pass_code, function(data){
+  			$scope.survey = data;
+  			console.log(data);
+  		});
+
+	}
+	
 })
+
 .controller('SurveyCtrl', function($scope) {
 
 })
